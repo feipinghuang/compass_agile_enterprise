a4b90df02c5e5f34ab924d4e86bde7a9edd68316
Gourav Modi <gourav1711@gmail.com>
2015-07-09 19:22:43 +0530

    categorized skill type and position type

--- erp_work_effort/app/models/skill_type.rb
+++ erp_work_effort/app/models/skill_type.rb
@@ -7,4 +7,9 @@ class SkillType < ActiveRecord::Base
   has_many  :party_skills
   has_many  :parties, :through => :party_skills
 
+  class << self
+	  def iid(internal_identifier)
+	    find_by_internal_identifier(internal_identifier)
+	  end
+	end
 end
