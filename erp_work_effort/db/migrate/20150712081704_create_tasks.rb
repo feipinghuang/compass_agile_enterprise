eda15e6ffadf23674cf615be3d96ec27dcaab83b
Jim Laskowski <jlaskowski@truenorthtechnology.com>
2015-07-19 13:59:42 -0400

    - adding task type to tasks

--- erp_work_effort/db/migrate/20150712081704_create_tasks.rb
+++ erp_work_effort/db/migrate/20150712081704_create_tasks.rb
@@ -7,9 +7,27 @@ class CreateTasks < ActiveRecord::Migration
         t.text     :content
         t.datetime :start
         t.datetime :end
+        t.column   :task_type_id, :integer
         t.column   :project_id, :integer
         t.column   :task_record_id, :integer
         t.column   :task_record_type, :string
+        t.timestamps
+      end
+    end
+
+    unless table_exists?(:task_types)
+      create_table :task_types do |t|
+        t.integer :parent_id
+        t.integer :lft
+        t.integer :rgt
+
+        #custom columns go here
+        t.string :description
+        t.string :comments
+        t.string :internal_identifier
+        t.string :external_identifier
+        t.string :external_id_source
+
         t.timestamps
       end
     end
@@ -19,6 +37,9 @@ class CreateTasks < ActiveRecord::Migration
   def self.down
     if table_exists?(:tasks)
       drop_table :tasks
+    end
+    if table_exists?(:task_types)
+      drop_table :task_types
     end
   end
 
