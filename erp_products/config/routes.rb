c6086411d47510ee1d2486bc66dcce44e8c71d54
Russell Holmes <russellfholmes@gmail.com>
2015-07-19 15:16:28 -0400

    moved product_types to api

--- erp_products/config/routes.rb
+++ erp_products/config/routes.rb
@@ -1,12 +1,20 @@
+Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+
+      resources :product_types
+
+    end
+  end
+
+end
+
 ErpProducts::Engine.routes.draw do
-  #product_manager
-  match '/erp_app/desktop/product_manager(/:action(/:id))' => 'erp_app/desktop/product_manager/base'
 
   namespace 'shared' do
-    resources 'product_types'
-    get 'product_types/show_details(/:id)' => 'product_types#show_details'
-    get 'product_types/show_details(/:id)' => 'product_types#show_details'
     resources :product_features, except: [:show]
     get '/product_features/get_values' => 'product_features#get_values'
   end
+
 end
