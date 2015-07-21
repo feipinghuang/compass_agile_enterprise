f0b99b763eb920c4bd2d786dc617ee7599f01abf
Russell Holmes <russellfholmes@gmail.com>
2015-07-21 09:48:10 -0400

    added from to WebsiteInquiry email

--- knitkit/app/mailers/website_inquiry_mailer.rb
+++ knitkit/app/mailers/website_inquiry_mailer.rb
@@ -1,11 +1,11 @@
 class WebsiteInquiryMailer < ActionMailer::Base
+  default :from => ErpTechSvcs::Config.email_notifications_from
 
   def inquiry(website_inquiry)
     subject = "#{website_inquiry.website.title} Inquiry"
     @website_inquiry = website_inquiry
 
     mail(:to => website_inquiry.website.configurations.first.get_item(ConfigurationItemType.find_by_internal_identifier('contact_us_email_address')).options.first.value,
-         :from => website_inquiry.email,
          :subject => subject,
          :content_type => 'text/html'
     )
