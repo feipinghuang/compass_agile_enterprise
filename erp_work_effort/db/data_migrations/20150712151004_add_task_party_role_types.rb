1487b26d69ed9f05a3535da1de0bbba0616a30d2
Jim Laskowski <jlaskowski@truenorthtechnology.com>
2015-07-22 00:09:18 -0400

    - fix project and task role type migrations

--- erp_work_effort/db/data_migrations/20150712151004_add_task_party_role_types.rb
+++ erp_work_effort/db/data_migrations/20150712151004_add_task_party_role_types.rb
@@ -1,27 +1,17 @@
 class AddTaskPartyRoleTypes
 
   def self.up
-
-    task_role_type = RoleType.where("internal_identifier = 'task' and parent_id is null").first
-    if task_role_type.nil?
-      task_role_type = RoleType.create(description: 'Task', internal_identifier: 'task')
-    end
-
-    if  RoleType.where("internal_identifier = 'assignee' and parent_id = ?", task_role_type.id).first.nil?
-      assignee_role_type = RoleType.create(description: 'Assignee', internal_identifier: 'assignee')
-      assignee_role_type.move_to_child_of(task_role_type)
-    end
+    task_role_type = RoleType.find_or_create("task", "Task", RoleType.iid("application_composer"))
+    RoleType.find_or_create("assignee", "Assignee", task_role_type)
+    RoleType.find_or_create("developer", "Developer", task_role_type)
+    RoleType.find_or_create("business_analyst", "Business Analyst", task_role_type)
+    RoleType.find_or_create("quality_assurance", "Quality Assurance", task_role_type)
+    RoleType.find_or_create("project_manager", "Project Manager", task_role_type)
   end
 
   def self.down
-    task_role_type = RoleType.where("internal_identifier = 'task' and parent_id is null").first
-    unless task_role_type.nil?
-      assignee_role_type = RoleType.where("internal_identifier = 'assignee' and parent_id = ?", task_role_type.id).first
-      unless assignee_role_type.nil?
-        EntityPartyRole.destroy_all("where role_type_id = ?", assignee_role_type.id)
-        assignee_role_type.destroy
-      end
-    end
+    task_role_type = RoleType.find_or_create("task", "Task", RoleType.iid("application_composer"))
+    task_role_type.destroy
   end
 
 end
--- erp_work_effort/db/data_migrations/20150718054404_add_project_party_role_types.rb
+++ erp_work_effort/db/data_migrations/20150718054404_add_project_party_role_types.rb
@@ -1,27 +1,13 @@
 class AddProjectPartyRoleTypes
 
   def self.up
-
-    project_role_type = RoleType.where("internal_identifier = 'project' and parent_id is null").first
-    if project_role_type.nil?
-      project_role_type = RoleType.create(description: 'Project', internal_identifier: 'project')
-    end
-
-    if  RoleType.where("internal_identifier = 'assignee' and parent_id = ?", project_role_type.id).first.nil?
-      assignee_role_type = RoleType.create(description: 'Assignee', internal_identifier: 'assignee')
-      assignee_role_type.move_to_child_of(project_role_type)
-    end
+    project_role_type = RoleType.find_or_create("project", "Project", RoleType.iid("application_composer"))
+    RoleType.find_or_create("assignee", "Assignee", project_role_type)
   end
 
   def self.down
-    project_role_type = RoleType.where("internal_identifier = 'project' and parent_id is null").first
-    unless project_role_type.nil?
-      assignee_role_type = RoleType.where("internal_identifier = 'assignee' and parent_id = ?", project_role_type.id).first
-      unless assignee_role_type.nil?
-        EntityPartyRole.destroy_all("where role_type_id = ?", assignee_role_type.id)
-        assignee_role_type.destroy
-      end
-    end
+    project_role_type = RoleType.find_or_create("project", "Project", RoleType.iid("application_composer"))
+    project_role_type.destroy
   end
 
 end
