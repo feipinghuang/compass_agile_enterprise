7bd7cd91f364b9f0dd061962bd50a74dfca7b109
Jim Laskowski <jlaskowski@truenorthtechnology.com>
2015-07-27 20:07:59 -0400

    - added DefaultNestedSetMethods to NoteType and RoleType - removed redundant iid method from NoteType - added is_descendant_of? method to DefaultNestedSetMethods so you can tell whether someone, for example, is an employee, regardless of whether they have the employee role or a child role of employee

--- erp_base_erp_svcs/app/models/note_type.rb
+++ erp_base_erp_svcs/app/models/note_type.rb
@@ -2,12 +2,10 @@ class NoteType < ActiveRecord::Base
   attr_protected :created_at, :updated_at
   
   acts_as_nested_set
+  include ErpTechSvcs::Utils::DefaultNestedSetMethods
   acts_as_erp_type
 
   belongs_to :note_type_record, :polymorphic => true
   has_many   :notes
 
-  def self.iid(internal_identifier)
-    find_by_internal_identifier(internal_identifier)
-  end
 end
