3ede8fda81a18dbff4db1354f7cf3a5f069b59fb
Russell Holmes <russellfholmes@gmail.com>
2015-09-08 13:52:52 -0400

    removed old task application js files and images

--- erp_work_effort/lib/erp_work_effort/engine.rb
+++ erp_work_effort/lib/erp_work_effort/engine.rb
@@ -2,11 +2,6 @@ module ErpWorkEffort
   class Engine < Rails::Engine
     isolate_namespace ErpWorkEffort
 
-    initializer :assets do |config|
-      Rails.application.config.assets.paths << root.join("app", "assets", "images")
-      Rails.application.config.assets.precompile += %w{ erp_app/desktop/applications/tasks/app.js }
-    end
-
     ActiveSupport.on_load(:active_record) do
       include ErpWorkEffort::Extensions::ActiveRecord::ActsAsWorkEffort
       include ErpWorkEffort::Extensions::ActiveRecord::ActsAsRoutable
