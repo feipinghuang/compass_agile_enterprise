7a7f7cf286b77f4869311242d8797e252ce17028
Russell Holmes <russellfholmes@gmail.com>
2015-10-12 20:57:19 -0400

    added datetime field

--- erp_app/app/assets/javascripts/erp_app/shared/erp_app_shared.js
+++ erp_app/app/assets/javascripts/erp_app/shared/erp_app_shared.js
@@ -10,4 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require ./TimePickerField
+//= require ./DateTimePicker
+//= require ./DateTimeField
 //= require_tree .
