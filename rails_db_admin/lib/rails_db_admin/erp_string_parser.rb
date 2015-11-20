2fef259a1624eaeba9a8f64c21059a07bb7e2df2
Anup Biswal <anup.craver@gmail.com>
2015-11-20 20:40:09 +0530

    fixed bug report params not getting imported, handled report query errors gracefully

--- rails_db_admin/lib/rails_db_admin/erp_string_parser.rb
+++ rails_db_admin/lib/rails_db_admin/erp_string_parser.rb
@@ -4,21 +4,25 @@ module RailsDbAdmin
     
     class << self
       def render(str, options = {})
-        locals = options[:locals] || {}
+        begin
+          locals = options[:locals] || {}
 
-        # convert all variables to underscore as
-        # ERB parser has issues with capitalized variables
-        _str = str.gsub(ERB_REGEX) do |w|
-          w.delete(' ').underscore 
+          # convert all variables to underscore as
+          # ERB parser has issues with capitalized variables
+          _str = str.gsub(ERB_REGEX) do |w|
+            w.delete(' ').underscore 
+          end
+          # convert all to locals to underscore
+          _locals = {}
+          locals.each do |k,v|
+            _locals[k.to_s.delete(' ').underscore] = v
+          end
+          
+          query_params = RailsDbAdmin::QueryParams.new(_locals)
+          ERB.new(_str).result(query_params.get_binding)
+        rescue
+          nil
         end
-        # convert all to locals to underscore
-        _locals = {}
-        locals.each do |k,v|
-          _locals[k.to_s.delete(' ').underscore] = v
-        end
-        
-        query_params = RailsDbAdmin::QueryParams.new(_locals)
-        ERB.new(_str).result(query_params.get_binding)
       end
     end
     
