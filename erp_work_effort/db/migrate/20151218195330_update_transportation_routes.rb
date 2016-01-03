4e0dde0d6b8f6dff0742b279d6bbf7091cef28e3
Russell Holmes <russellfholmes@gmail.com>
2016-01-03 16:42:19 -0500

    Updated Transportation route migration Updated has_contacts to raise an error if updating fails

--- erp_work_effort/db/migrate/20151218195330_update_transportation_routes.rb
+++ erp_work_effort/db/migrate/20151218195330_update_transportation_routes.rb
@@ -4,6 +4,11 @@ class UpdateTransportationRoutes < ActiveRecord::Migration
       add_index :transportation_route_stops, :transportation_route_id, name: 'trans_route_stop_trans_route_idx'
     end
 
+    unless column_exists? :transportation_routes, :billable
+      add_column :transportation_routes, :billable, :boolean, default: false
+      add_column :transportation_routes, :manual_entry, :boolean, default: true
+    end
+
     unless index_exists? :transportation_route_segments, 'trans_route_seg_trans_route_id_idx'
       add_index :transportation_route_segments, :transportation_route_id, name: 'trans_route_seg_trans_route_id_idx'
       add_index :transportation_route_segments, :from_transportation_route_stop_id, name: 'trans_route_seg_from_trans_stop_idx'
@@ -12,7 +17,7 @@ class UpdateTransportationRoutes < ActiveRecord::Migration
 
     unless column_exists? :transportation_route_segments, :miles_traveled
       rename_column :transportation_route_segments, :end_milage, :end_mileage
-      change_column :transportation_route_segments, :end_mileage,:decimal, :precision => 8, :scale => 1
+      change_column :transportation_route_segments, :end_mileage, :decimal, :precision => 8, :scale => 1
 
       change_column :transportation_route_segments, :start_mileage, :decimal, :precision => 8, :scale => 1
       add_column :transportation_route_segments, :miles_traveled, :decimal, :precision => 8, :scale => 1
@@ -22,19 +27,24 @@ class UpdateTransportationRoutes < ActiveRecord::Migration
   end
 
   def down
-    if index_exists? :transportation_route_stops, 'trans_route_stop_trans_route_idx'
-      remove_index :transportation_route_id, name: 'trans_route_stop_trans_route_idx'
+    if index_exists? :transportation_route_stops, :transportation_route_id, name: 'trans_route_stop_trans_route_idx'
+      remove_index :transportation_route_stops, name: 'trans_route_stop_trans_route_idx'
     end
 
-    if index_exists? :transportation_route_segments, 'trans_route_seg_trans_route_id_idx'
+    if column_exists? :transportation_routes, :billable
+      remove_column :transportation_routes, :billable
+      remove_column :transportation_routes, :manual_entry
+    end
+
+    if index_exists? :transportation_route_segments, :transportation_route_id, name: 'trans_route_seg_trans_route_id_idx'
       remove_index :transportation_route_segments, name: 'trans_route_seg_trans_route_id_idx'
       remove_index :transportation_route_segments, name: 'trans_route_seg_from_trans_stop_idx'
       remove_index :transportation_route_segments, name: 'trans_route_seg_to_trans_stop_idx'
     end
 
     if column_exists? :transportation_route_segments, :miles_traveled
-      rename_column :transportation_route_segments, :end_mileage, :end_mileage
-      change_column :transportation_route_segments, :end_mileage, :integer
+      rename_column :transportation_route_segments, :end_mileage, :end_milage
+      change_column :transportation_route_segments, :end_milage, :integer
 
       change_column :transportation_route_segments, :start_mileage, :integer
       remove_column :transportation_route_segments, :miles_traveled
