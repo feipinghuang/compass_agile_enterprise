24be72fae16035340961cc8edb74332ab6dbd3a1
Russell Holmes <russellfholmes@gmail.com>
2016-01-06 15:57:05 -0500

    formatting cleanup

--- erp_base_erp_svcs/app/models/email_address.rb
+++ erp_base_erp_svcs/app/models/email_address.rb
@@ -10,7 +10,9 @@ class EmailAddress < ActiveRecord::Base
 
   has_contact
 
-  validates_format_of :email_address, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :message => "Must be a valid email address"
+  validates_format_of :email_address,
+                      with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
+                      message: "Must be a valid email address"
 
   def summary_line
     "#{description} : #{email_address}"
