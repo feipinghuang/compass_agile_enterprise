9bf2acc225c6a9c8f3e16ea9b666e5807f9ae180
Russell Holmes <russellfholmes@gmail.com>
2016-05-24 13:28:50 -0400

    added helper method to get the count of product types for a category

--- erp_products/app/models/extensions/cateogry.rb
+++ erp_products/app/models/extensions/cateogry.rb
@@ -1,3 +1,21 @@
+Category.class_eval do
+
+  # Count of ProductTypes classified by this category
+  #
+  def product_type_count(context={})
+    statement = CategoryClassification.joins("inner join product_types on product_types.id = category_classifications.classification_id
+                                             and category_classifications.classification_type = 'ProductType'")
+                                      .where(category_id: self.id)
+
+   if context[:only_available_on_web]
+     statement = statement.where(product_types: {available_on_web: true})
+   end                                 
+   
+   statement.count('category_classifications.id')                                  
+  end
+
+end
+
 module ErpProducts
   module Extensions
 
@@ -6,8 +24,7 @@ module ErpProducts
       def to_data_hash
         data = super
 
-        data[:product_type_count] = ::CategoryClassification.where('classification_type = ?', 'ProductType').
-                                        where(category_id: self.id).count('id')
+        data[:product_type_count] = product_type_count
 
         data
       end
