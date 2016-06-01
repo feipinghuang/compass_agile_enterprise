bf7991f16bab0871ecd462403e0a2484c07af399
Russell Holmes <russellfholmes@gmail.com>
2016-06-01 09:00:07 -0400

    cleaned up action view extensions for erp_app

--- erp_app/lib/erp_app/extensions/railties/action_view/base.rb
+++ erp_app/lib/erp_app/extensions/railties/action_view/base.rb
@@ -1,12 +1,36 @@
-#require all ActionView helper files
-Dir.entries(File.join(File.dirname(__FILE__),"helpers")).delete_if{|name| name =~ /^\./}.each do |file|
-  require "erp_app/extensions/railties/action_view/helpers/#{file}"
-end
+require "erp_app/extensions/railties/action_view/helpers/include_helper"
 
 ActionView::Base.class_eval do
   include ErpApp::Extensions::Railties::ActionView::Helpers::IncludeHelper
 end
 
+ActionView::Helpers::FormTagHelper.class_eval do
+
+  def form_remote_tag(url, options={}, &block)
+    #add ajax_replace class
+    options[:class].nil? ? 'ajax_replace' : "#{options[:class]} ajax_replace"
+    #add remote => true to options
+    options.merge!({:remote => true})
+
+    if block_given?
+      form_tag url, options do
+        yield
+      end
+    else
+      form_tag url, options
+    end
+  end
+
+end
+
 ActionView::Helpers::TagHelper.class_eval do
-  include ErpApp::Extensions::Railties::ActionView::Helpers::RemoteTagHelper
+
+  def link_to_remote(name, url, options={})
+    #add ajax_replace class
+    options[:class].nil? ? 'ajax_replace' : "#{options[:class]} ajax_replace"
+    #add remote => true to options
+    options.merge!({:remote => true})
+    link_to name, url, options
+  end
+
 end
