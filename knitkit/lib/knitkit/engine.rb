745618c1c921705fd6f74613cb34885d06c6ea64
Anup Biswal <anup.craver@gmail.com>
2017-02-21 15:27:20 +0530

    fixed typo in knitkit engine.rb

--- knitkit/lib/knitkit/engine.rb
+++ knitkit/lib/knitkit/engine.rb
@@ -32,11 +32,12 @@ module Knitkit
 
       Rails.application.config.assets.paths << root.join("app", "assets", "images")
       Rails.application.config.assets.precompile += %w{ knitkit-web.css knitkit/custom.css knitkit-web.js knitkit/theme.js }
-      Rails.application.config.assets.precompile += %w{ knitkit/website_builder/medium-editor.min.js knitkit/website_builder/medium-bootstrap.js knitkit/website_builder/medium_editor_theme/default.min.js }
       Rails.application.config.assets.precompile += %w{ erp_app/desktop/applications/knitkit/app.js }
       Rails.application.config.assets.precompile += %w{ erp_app/desktop/applications/knitkit/app.css }
       
       Rails.application.config.assets.precompile += %w{ erp_app/shared/knitkit_shared.css }
+      Rails.application.config.assets.precompile += %w{ knitkit/website_builder/medium-editor.min.css knitkit/website_builder/medium-bootstrap.css knitkit/website_builder/medium_editor_theme/default.min.css }
+
       ErpApp::Config.shared_css_assets += %w{ erp_app/shared/knitkit_shared.css }
     end
 
