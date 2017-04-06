78931c9a170ebf3922057c5a0b134be5f966d02d
Russell Holmes <russellfholmes@gmail.com>
2017-04-05 21:22:59 -0400

    added updated_contents! and rename! to file_assets

--- erp_tech_svcs/app/models/file_asset.rb
+++ erp_tech_svcs/app/models/file_asset.rb
@@ -246,6 +246,26 @@ class FileAsset < ActiveRecord::Base
     super attributes.merge(:directory => directory, :name => name, :data => data)
   end
 
+  def update_contents!(contents)
+    self.data = contents
+    self.save!
+  end
+
+  def rename!(new_name)
+    file_support = ErpTechSvcs::FileSupport::Base.new(:storage => ErpTechSvcs::Config.file_storage)
+
+    result, message = file_support.rename_file(File.join(file_support.root, directory, name), new_name)
+
+    if result
+      self.name = new_name
+      self.save!
+
+      return true, nil
+    else
+      return false, message
+    end
+  end
+
   def fully_qualified_url
     case ErpTechSvcs::Config.file_storage
       when :filesystem
