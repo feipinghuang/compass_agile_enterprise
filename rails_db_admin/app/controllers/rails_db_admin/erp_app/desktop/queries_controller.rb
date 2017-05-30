b800ffa811820b413aed376db4842bdf7dbe4085
Russell Holmes <russellfholmes@gmail.com>
2017-05-30 19:13:25 -0400

    added the ability to export queries to csv

--- rails_db_admin/app/controllers/rails_db_admin/erp_app/desktop/queries_controller.rb
+++ rails_db_admin/app/controllers/rails_db_admin/erp_app/desktop/queries_controller.rb
@@ -92,7 +92,6 @@ module RailsDbAdmin
 
         def execute_query
           begin
-
             sql = params[:sql]
             selection = params[:selected_sql]
             sql = sql.rstrip
@@ -132,20 +131,20 @@ module RailsDbAdmin
               #at the beginning of the text area
               if cursor_pos <= sql_stmt_arry[0].fetch(:begin)
                 sql_str = sql_arr.values_at(sql_stmt_arry[0].fetch(:begin)..
-                                                sql_stmt_arry[0].fetch(:end)).join(" ")
+                                            sql_stmt_arry[0].fetch(:end)).join(" ")
                 #run the last query if we're in whitespace at the end of the
                 #textarea
               elsif cursor_pos > sql_stmt_arry[last_sql_stmt].fetch(:begin)
                 sql_str = sql_arr.values_at(
-                    sql_stmt_arry[last_sql_stmt].fetch(:begin)..
-                        sql_stmt_arry[last_sql_stmt].fetch(:end)).join(" ")
+                  sql_stmt_arry[last_sql_stmt].fetch(:begin)..
+                sql_stmt_arry[last_sql_stmt].fetch(:end)).join(" ")
                 #run query based on cursor position
               else
                 sql_stmt_arry.each do |sql_stmt|
                   if cursor_pos >= sql_stmt.fetch(:begin) &&
                       cursor_pos <= sql_stmt.fetch(:end)
                     sql_str = sql_arr.values_at(sql_stmt.fetch(:begin)..
-                                                    sql_stmt.fetch(:end)).join(" ")
+                                                sql_stmt.fetch(:end)).join(" ")
                   end
                 end
               end
@@ -180,6 +179,93 @@ module RailsDbAdmin
             ExceptionNotifier.notify_exception(ex) if defined? ExceptionNotifier
 
             render :json => {success: false, message: ex.message}
+          end
+        end
+
+        def download_csv
+          begin
+            sql = params[:sql]
+            selection = params[:selected_sql]
+            sql = sql.rstrip
+            cursor_pos = params[:cursor_pos].to_i
+
+            # append a semicolon as the last character if the
+            # user forgot
+            if !sql.end_with?(";")
+              sql << ";"
+            end
+
+            sql_arr = sql.split("\n")
+            sql_stmt_arry = []
+            sql_str = ""
+
+            #search for the query to run based on cursor position if there
+            #was nothing selected by the user
+            if (selection == nil || selection == "")
+              last_stmt_end = 0
+              sql_arr.each_with_index do |val, idx|
+                if val.match(';')
+                  sql_stmt_arry << {:begin => (sql_stmt_arry.length > 0) ? last_stmt_end + 1 : 0, :end => idx}
+                  last_stmt_end = idx
+                end
+              end
+
+              last_sql_stmt = sql_stmt_arry.length-1
+              #run the first complete query if we're in whitespace
+              #at the beginning of the text area
+              if cursor_pos <= sql_stmt_arry[0].fetch(:begin)
+                sql_str = sql_arr.values_at(sql_stmt_arry[0].fetch(:begin)..
+                                            sql_stmt_arry[0].fetch(:end)).join(" ")
+                #run the last query if we're in whitespace at the end of the
+                #textarea
+              elsif cursor_pos > sql_stmt_arry[last_sql_stmt].fetch(:begin)
+                sql_str = sql_arr.values_at(
+                  sql_stmt_arry[last_sql_stmt].fetch(:begin)..
+                sql_stmt_arry[last_sql_stmt].fetch(:end)).join(" ")
+                #run query based on cursor position
+              else
+                sql_stmt_arry.each do |sql_stmt|
+                  if cursor_pos >= sql_stmt.fetch(:begin) &&
+                      cursor_pos <= sql_stmt.fetch(:end)
+                    sql_str = sql_arr.values_at(sql_stmt.fetch(:begin)..
+                                                sql_stmt.fetch(:end)).join(" ")
+                  end
+                end
+              end
+            else
+              sql_str = selection
+            end
+
+            columns, values, exception = @query_support.execute_sql(sql_str)
+
+            if exception.nil?
+              csv_data = CSV.generate do |csv|
+                csv << columns
+
+                values.collect(&:values).each do |row|
+                  csv << row
+                end
+              end
+
+              send_data csv_data,
+                  filename: 'query_results.csv',
+                  type: 'application/csv'
+            else
+              send_data 'Error executing sql',
+                filename: 'query_results.error',
+                type: 'text'
+            end
+
+          rescue StandardError => ex
+            Rails.logger.error ex.message
+            Rails.logger.error ex.backtrace.join("\n")
+
+            # email notification
+            ExceptionNotifier.notify_exception(ex) if defined? ExceptionNotifier
+
+            send_data 'Error executing sql',
+              filename: 'query_results.error',
+              type: 'text'
           end
         end
 
