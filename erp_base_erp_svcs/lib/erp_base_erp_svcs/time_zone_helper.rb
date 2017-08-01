4bc843cd4c202a6bed40e6599547875cea3a1067
Gourav Modi <gourav1711@gmail.com>
2017-08-01 19:36:37 +0530

    Added in_utc_time to time_helper

--- erp_base_erp_svcs/lib/erp_base_erp_svcs/time_zone_helper.rb
+++ erp_base_erp_svcs/lib/erp_base_erp_svcs/time_zone_helper.rb
@@ -32,6 +32,14 @@ module ErpBaseErpSvcs
           (::Time.now  + @offset_in_hours.hours).end_of_week
         end
 
+        # Finds the correspoding time in UTC combining the date and time passed separately
+        def in_utc_time(date, time)
+          parsed_date = (Time.parse(date)) - client_utc_offset.hours
+          parsed_time = Time.parse(time)
+          datetime_in_utc = parsed_date.change(hour: parsed_time.hour, min: parsed_time.min) + client_utc_offset.hours
+          Time.zone.local_to_utc (datetime_in_utc)
+        end
+
         protected
 
         def get_offset_in_hours(client_utc_offset=nil)
