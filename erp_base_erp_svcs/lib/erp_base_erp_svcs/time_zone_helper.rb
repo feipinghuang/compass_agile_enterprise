afe6b16b78c280116fbfe168fce6535a6ac585ed
Gourav Modi <gourav1711@gmail.com>
2017-08-02 14:36:53 +0530

    Replaced Time with ::Time in time helper

--- erp_base_erp_svcs/lib/erp_base_erp_svcs/time_zone_helper.rb
+++ erp_base_erp_svcs/lib/erp_base_erp_svcs/time_zone_helper.rb
@@ -34,10 +34,10 @@ module ErpBaseErpSvcs
 
         # Finds the correspoding time in UTC combining the date and time passed separately
         def in_utc_time(date, time)
-          parsed_date = (Time.parse(date)) - @offset_in_hours.hours
-          parsed_time = Time.parse(time)
+          parsed_date = (::Time.parse(date)) - @offset_in_hours.hours
+          parsed_time = ::Time.parse(time)
           datetime_in_utc = parsed_date.change(hour: parsed_time.hour, min: parsed_time.min) + @offset_in_hours.hours
-          Time.zone.local_to_utc (datetime_in_utc)
+          ::Time.zone.local_to_utc (datetime_in_utc)
         end
 
         protected
