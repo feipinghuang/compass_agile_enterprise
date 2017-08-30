ac98bfb58335b4028c3525e186efe578b6fe3337
Suman Ranjan Panda <sumanranjanpanda@gmail.com>
2017-08-30 16:10:44 +0530

    Added check for both nil & blank

--- knitkit/app/controllers/knitkit/website_sections_controller.rb
+++ knitkit/app/controllers/knitkit/website_sections_controller.rb
@@ -4,10 +4,10 @@ module Knitkit
     def index
       @contents = Component.find_published_by_section(@active_publication, @website_section)
       layout = @website_section.get_published_layout(@active_publication)
-      
+
       if params[:is_mobile]
-        layout.nil? ? (render :layout => false) : (render :inline => layout, :layout => false)
-      elsif layout.nil?
+        layout.blank? ? (render :layout => false) : (render :inline => layout, :layout => false)
+      elsif layout.blank?
         @website_section.render_base_layout? ? (render) : (render :layout => false)
       else
         @website_section.render_base_layout? ? (render :inline => layout, :layout => 'knitkit/base') : (render :inline => layout)
