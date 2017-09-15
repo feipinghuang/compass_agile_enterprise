c9ca75adf95972213b2288731c860f9fc2943074
Suman Ranjan Panda <sumanranjanpanda@gmail.com>
2017-09-15 18:15:36 +0530

    Added Contact us widgets spec

--- knitkit/spec/factories/website.rb
+++ knitkit/spec/factories/website.rb
@@ -3,4 +3,21 @@ FactoryGirl.define do
     w.title "Some Title!"
     w.sequence(:internal_identifier) {|n| "website-#{n}"}
   end
+
+  trait :configure_with_host do
+    transient do
+      host 'localhost:3000'
+    end
+    after :create do |website, options|
+      FactoryGirl.create(:website_party_role,
+                         website: website,
+                         party: Party.find_by_description('CompassAE'),
+                         role_type: RoleType.iid('dba_org'))
+
+      website.hosts << WebsiteHost.create(:host => options.host)
+      website.configurations.first.update_configuration_item(ConfigurationItemType.find_by_internal_identifier('primary_host'), options.host)
+      website.save
+    end
+  end
+
 end
