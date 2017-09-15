75b5575ddee4dd4d788091a73bbed8cdb06f37a6
Suman Ranjan Panda <sumanranjanpanda@gmail.com>
2017-09-15 20:40:09 +0530

    Refactored signup widget spec

--- knitkit/spec/widgets/signup/signup_widget_spec.rb
+++ knitkit/spec/widgets/signup/signup_widget_spec.rb
@@ -5,98 +5,72 @@ describe Widgets::Signup::Base, type: :controller do
   #create dummy controller
   controller(ApplicationController) do end
 
-  before(:all) do
-    @website = FactoryGirl.create(:website, name: "Test Website")
+  let(:website) { FactoryGirl.create(:website, :configure_with_host, name: 'Test Website', host: 'localhost:3000') }
+  let(:user_valid_params) { { first_name: 'Test', last_name: 'Test', email: 'test@example.com', username: 'test', password: 'password', password_confirmation: 'password' } }
+  let(:user_invalid_params) { { first_name: '', last_name: '', email: '', username: '', password: '', password_confirmation: '' } }
+  let(:role_type) { FactoryGirl.create(:role_type) }
+  let(:user) { FactoryGirl.create(:user) }
 
-    FactoryGirl.create(:website_party_role,
-                       website: @website,
-                       party: Party.find_by_description('CompassAE'),
-                       role_type: RoleType.iid('dba_org'))
-
-    @website.hosts << WebsiteHost.create(:host => 'localhot:3000')
-    @website.configurations.first.update_configuration_item(ConfigurationItemType.find_by_internal_identifier('primary_host'), 'localhot:3000')
-    @website.save
-
-    @website.hosts << FactoryGirl.create(:website_host)
-    @role_type = FactoryGirl.create(:role_type)
-    @user = FactoryGirl.create(:user)
-  end
-
-  before(:each) do
-    @user_data = {
-      first_name: 'Test',
-      last_name: 'Test',
-      email: 'Test@Test.com',
-      username: 'test',
-      password: 'password',
-      password_confirmation: 'password',
-    }
-  end
-
-  describe "Get index" do
+  describe "GET #index" do
     it "returns index" do
       uuid = Digest::SHA1.hexdigest(Time.now.to_s + rand(10000).to_s)
-      allow(Website).to receive(:find_by_host).and_return(@website)
-
+      allow(Website).to receive(:find_by_host).and_return(website)
       login_url = {login_url: "/login"}
       widget = Widgets::Signup::Base.new(controller, "signup", :index, uuid, login_url, nil)
-
       result = widget.process('index')
-      expect(result).to match(/class="form-signin"/)
+
+      expect(result).not_to be_empty
+      expect(result).to render_template(:index)
     end
   end
 
-  describe "Get new" do
+  describe "POST #new" do
+    context 'with valid data' do
+      it "should create user" do
+        uuid = Digest::SHA1.hexdigest(Time.now.to_s + rand(10000).to_s)
+        allow(Website).to receive(:find_by_host).and_return(website)
+        widget = Widgets::Signup::Base.new(controller, "signup", :new, uuid, user_valid_params, nil)
 
-    it "should create user" do
-      uuid = Digest::SHA1.hexdigest(Time.now.to_s + rand(10000).to_s)
-      allow(Website).to receive(:find_by_host).and_return(@website)
+        expect { widget.process('new') }.to change { User.count }.by(1)
+      end
+    end
 
-      widget = Widgets::Signup::Base.new(controller, "signup", :new, uuid, @user_data, nil)
+    context 'with existing valid user data' do
+      it "should not create user" do
+        uuid = Digest::SHA1.hexdigest(Time.now.to_s + rand(10000).to_s)
+        allow(Website).to receive(:find_by_host).and_return(website)
+        user_valid_params[:email] = user.email
+        user_valid_params[:username] = user.username
 
-      expect{
+        widget = Widgets::Signup::Base.new(controller, "signup", :new, uuid, user_valid_params, nil)
+
+        expect { widget.process('new') }.not_to change(User, :count)
+      end
+    end
+
+    context 'with default RoleType' do
+      it "should create user " do
+        uuid = Digest::SHA1.hexdigest(Time.now.to_s + rand(10000).to_s)
+        allow(Website).to receive(:find_by_host).and_return(website)
+
+        widget = Widgets::Signup::Base.new(controller, "signup", :new, uuid, user_valid_params, nil)
         widget.process('new')
-      }.to change {User.count}
+        expect(User.last.party.role_types).not_to match_array([])
+      end
     end
 
-    it "should not create duplicate user" do
-      uuid = Digest::SHA1.hexdigest(Time.now.to_s + rand(10000).to_s)
-      allow(Website).to receive(:find_by_host).and_return(@website)
+    context 'with specified RoleType' do
+      it "should create user " do
+        uuid = Digest::SHA1.hexdigest(Time.now.to_s + rand(10000).to_s)
+        allow(Website).to receive(:find_by_host).and_return(website)
 
-      @user_data[:email] = @user.email,
-      @user_data[:username] = @user.username,
+        user_valid_params[:party_roles] = role_type.internal_identifier
 
-      widget = Widgets::Signup::Base.new(controller, "signup", :new, uuid, @user_data, nil)
+        widget = Widgets::Signup::Base.new(controller, "signup", :new, uuid, user_valid_params, nil)
 
-      expect {
         widget.process('new')
-      }.not_to change {User.count}
+        expect(User.last.party.role_types.first.internal_identifier).to eq(role_type.internal_identifier)
+      end
     end
-
-    it "should create user with default RoleType" do
-      uuid = Digest::SHA1.hexdigest(Time.now.to_s + rand(10000).to_s)
-      allow(Website).to receive(:find_by_host).and_return(@website)
-
-      widget = Widgets::Signup::Base.new(controller, "signup", :new, uuid, @user_data, nil)
-      widget.process('new')
-      expect(User.last.party.role_types).not_to match_array([])
-    end
-
-    it "should create user with specified RoleType" do
-      uuid = Digest::SHA1.hexdigest(Time.now.to_s + rand(10000).to_s)
-      allow(Website).to receive(:find_by_host).and_return(@website)
-
-      @user_data[:party_roles] = @role_type.internal_identifier
-
-      widget = Widgets::Signup::Base.new(controller, "signup", :new, uuid, @user_data, nil)
-
-      widget.process('new')
-      expect(User.last.party.role_types.first.internal_identifier).to eq(@role_type.internal_identifier)
-    end
-  end
-
-  after(:all) do
-    @website.destroy if @website
-    @user.destroy if @user
   end
 end
