d846cc981e0b956eee97f949e21c34370b1b23e2
vivek <vivek5110069@gmail.com>
2017-09-19 18:56:44 +0530

    updated to use trait in website factory

--- knitkit/spec/widgets/reset_password/reset_password_spec.rb
+++ knitkit/spec/widgets/reset_password/reset_password_spec.rb
@@ -6,18 +6,7 @@ describe Widgets::ResetPassword::Base, type: :controller do
   controller(ApplicationController) do end
 
   before(:all) do
-    @website = FactoryGirl.create(:website, name: "Test Website")
-
-    FactoryGirl.create(:website_party_role,
-                       website: @website,
-                       party: Party.find_by_description('CompassAE'),
-                       role_type: RoleType.iid('dba_org'))
-
-    @website.hosts << WebsiteHost.create(:host => 'localhot:3000')
-    @website.configurations.first.update_configuration_item(ConfigurationItemType.find_by_internal_identifier('primary_host'), 'localhot:3000')
-    @website.save
-
-    @website.hosts << FactoryGirl.create(:website_host)
+    @website = FactoryGirl.create(:website, :configure_with_host, name: 'Test Website', host: 'localhost:3000')
     @test_user = FactoryGirl.create(:user)
   end
 
@@ -29,7 +18,8 @@ describe Widgets::ResetPassword::Base, type: :controller do
       widget = Widgets::ResetPassword::Base.new(controller, "reset_password", :index, uuid, {}, nil)
 
       result = widget.process('index')
-      expect(result).to match(/id="reset_password_form"/)
+      expect(result).not_to be_empty
+      expect(result).to render_template(:index)
     end
 
     it "should verify reset password token" do
