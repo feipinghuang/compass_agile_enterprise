81fe7a961a9123c394fbf95cb60daa241e68b378
Anup Biswal <anup.craver@gmail.com>
2017-09-20 20:16:23 +0530

    fixed edges cases for span insertion in a content block

--- knitkit/app/assets/javascripts/erp_app/desktop/applications/knitkit/east_region/component_properties_panel.js
+++ knitkit/app/assets/javascripts/erp_app/desktop/applications/knitkit/east_region/component_properties_panel.js
@@ -173,6 +173,31 @@ Ext.define("Compass.ErpApp.Desktop.Applications.Knitkit.ComponentPropertiesFormP
     loadSelectedTextProperties: function(iframe) {
         var me = this;
         me.removeAll();
+
+        var selection = iframe.contentWindow.getSelection();
+
+        // find values for selection
+        var color = null,
+            backgroundColor = null,
+            fontSize = null,
+            fontFamily = null;
+        
+        if (selection.rangeCount > 0) {
+            var range = selection.getRangeAt(0),
+                startContainerStyle = range.startContainer.parentNode.style,
+                endContainerStyle = range.endContainer.parentNode.style;
+
+            // If the values are consistent through out the selection get them else get nothing
+            function getSelectionProperty(propName) {
+                return startContainerStyle[propName] == endContainerStyle[propName] ? startContainerStyle[propName] : null;
+            }
+            
+            color = getSelectionProperty('color');
+            backgroundColor = getSelectionProperty('backgroundColor');
+            fontSize = getSelectionProperty('fontSize');
+            fontFamily = getSelectionProperty('fontFamily');
+        }
+            
         me.add({
             xtype: 'form',
             autoScroll: true,
@@ -197,26 +222,59 @@ Ext.define("Compass.ErpApp.Desktop.Applications.Knitkit.ComponentPropertiesFormP
                         var properties = formPanel.getValues();
                         
                         // construct selected text style
-                        var style = ''; 
+                        var style = '';
                         for(var attr in properties) {
                             if(!Compass.ErpApp.Utility.isBlank(properties[attr]))
                                 style += attr + ':' + properties[attr] + '; ';
                         }
 
-                        // there is no direct way of inserting a span so the hack is insert an anchor tag
-                        // wrap it the a span and then remove the anchor tag
-                        var uniqueId = iframe.id + '-wrap-link';
-                        iframe.contentDocument.execCommand('CreateLink', false, uniqueId);
-                        var sel = $(iframe.contentDocument.body).find('.container > .row > .col-md-12').find('a[href="' + uniqueId + '"]');
-                        sel.wrap('<span style=' + style +'></span>');
-                        sel.contents().unwrap();
+                        var selection = iframe.contentWindow.getSelection();
                         
-                        me.down('#applyStatus').setText('Applied Successfully')
-                        me.down('#applyStatus').show();
-                        setTimeout(function(){
-                            me.down('#applyStatus').hide();
-                            me.down('#applyStatus').setText('');
-                        }, 3000)
+                        if (selection.rangeCount > 0) {
+
+                            var range = selection.getRangeAt(0),
+                                startContainer = range.startContainer,
+                                startParent = startContainer.parentNode,
+                                endContainer = range.endContainer,
+                                endParent = endContainer.parentNode;
+
+
+                            if (startContainer.nodeValue.trim() == endContainer.nodeValue.trim() &&
+                                endContainer.nodeValue.trim() == range.toString() &&
+                                startParent.isSameNode(endParent) &&
+                                startParent.nodeName == 'SPAN') {
+                                var existingStyle = startParent.style.cssText;
+                                startParent.style.cssText = existingStyle + style;
+
+                                me.cleanUpDuplicateStyles(startParent);
+                                me.unwrapBlankStyledSpan(startParent);
+                            } else {
+                                // there is no direct way of inserting a span so the hack is insert an anchor tag
+                                // wrap it the a span and then remove the anchor tag
+                                var uniqueId = iframe.id + '-wrap-link';
+                                iframe.contentDocument.execCommand('CreateLink', false, uniqueId);
+                                var sel = $(iframe.contentDocument.body).find('.container > .row > .col-md-12').find('a[href="' + uniqueId + '"]');
+                                sel.wrap('<span class="temp-internal-select" style="' + style +'"></span>');
+                                sel.contents().unwrap();
+                                $(iframe.contentDocument.body).find('.container > .row > .col-md-12').find(".temp-internal-select").each(function(){
+                                    me.cleanUpDuplicateStyles($(this)[0]);
+                                    $(this).removeAttr('class');
+                                    me.unwrapBlankStyledSpan($(this)[0]);
+                                });
+                            }
+
+                            
+                            // close the toolbar
+                            if (iframe.contentWindow.__pen__) iframe.contentWindow.__pen__._menu.style.display = 'none';
+
+                            me.down('#applyStatus').setText('Applied Successfully')
+                            me.down('#applyStatus').show();
+                            setTimeout(function(){
+                                me.down('#applyStatus').hide();
+                                me.down('#applyStatus').setText('');
+                            }, 3000)
+
+                        }
 
                     }
                 }
@@ -236,23 +294,47 @@ Ext.define("Compass.ErpApp.Desktop.Applications.Knitkit.ComponentPropertiesFormP
                 xtype: 'compassaecolorpicker',
                 fieldLabel: 'Color',
                 name: 'color',
+                value: color
             }, {
                 xtype: 'compassaecolorpicker',
                 fieldLabel: 'Background Color',
                 name: 'background',
+                value: backgroundColor
             }, {
                 xtype: 'textfield',
                 fieldLabel: 'Font Size',
                 regex: /^(\d)+(px)$/,
                 regexText: 'Invalid font size, Try something like 10px',
                 name: 'font-size',
+                value: fontSize
             }, {
                 xtype: 'textfield',
                 fieldLabel: 'Font Family',
                 name: 'font-family',
+                value: fontFamily
             }]
         });
+    },
+
+    cleanUpDuplicateStyles: function(node) {
+        if (!node.style) return;
+        Ext.each(node.style.cssText.split(';'), function(nodeStyle){
+            if (!Compass.ErpApp.Utility.isBlank(nodeStyle)) {
+                var styleFrags = nodeStyle.split(':');
+                $(node).find('[style*=' + styleFrags[0] + ']').each(function(){
+                    $(this).css(styleFrags[0], '');
+                });
+            }
+        });
+    },
+
+    unwrapBlankStyledSpan: function(node) {
+        $(node).find('[style=""]').each(function(){
+            $(this).contents().unwrap();
+        });
     }
+
+
 });
 
 
