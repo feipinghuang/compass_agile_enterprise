4d7325d71938bb0deb7f7c9bb55b317c554f8a05
rholmes <russellfholmes@gmail.com>
2017-11-07 22:00:21 -0500

    timezone fix

--- erp_base_erp_svcs/lib/erp_base_erp_svcs/time_zone_helper.rb
+++ erp_base_erp_svcs/lib/erp_base_erp_svcs/time_zone_helper.rb
@@ -9,11 +9,11 @@ module ErpBaseErpSvcs
         end
 
         def in_client_time(time)
-          time + @offset_in_hours.hours
+          ::Time.parse(time.strftime('%Y-%m-%d %H:%M:%S UTC')) + @offset_in_hours.hours
         end
 
         def client_to_utc_time(time)
-          time - @offset_in_hours.hours
+          ::Time.parse(time.strftime('%Y-%m-%d %H:%M:%S UTC')) - @offset_in_hours.hours
         end
 
         def beginning_of_day
