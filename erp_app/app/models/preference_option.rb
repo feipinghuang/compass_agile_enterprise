91dbaad801f9757014e0be5ec908673dced98c3b
rholmes <russellfholmes@gmail.com>
2017-11-19 10:09:27 -0500

    added value column to attr_accessible for PreferenceOption

--- erp_app/app/models/preference_option.rb
+++ erp_app/app/models/preference_option.rb
@@ -1,5 +1,5 @@
 class PreferenceOption < ActiveRecord::Base
-  attr_accessible :description, :internal_identifier
+  attr_accessible :description, :internal_identifier, :value
 
   has_many   :preferences
   has_and_belongs_to_many :preference_type
