151ab5c04d6eb63070687a1eb6e99233abb3832e
Gerry Boyd <gboyd@truenorthtechnology.com>
2017-11-22 09:36:43 -0500

    Changed name in spec file from ProductDiscountOffer to SimpleProductOffer

--- erp_products/spec/models/simple_product_offer_spec.rb
+++ erp_products/spec/models/simple_product_offer_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe ProductDiscountOffer do
+describe SimpleProductOffer do
   it "can be instantiated" do
-    ProductDiscountOffer.new.should be_an_instance_of(ProductDiscountOffer)
+    SimpleProductOffer.new.should be_an_instance_of(SimpleProductOffer)
   end
 
   it "can be saved successfully" do
-    ProductDiscountOffer.create().should be_persisted
+    SimpleProductOffer.create().should be_persisted
   end
 end
 
